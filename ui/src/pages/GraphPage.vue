3dc38f9a5fb64aba61f391ddb55f9c2af65429ca
Stanislav Poslavsky <poslavskysv@milaboratories.com>
2024-10-25 23:19:52 +0200

    wip

--- ui/src/pages/GraphPage.vue
+++ ui/src/pages/GraphPage.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
 import { GraphMaker, GraphMakerSettings } from "@milaboratories/graph-maker";
-import '@milaboratories/graph-maker/dist/style.css';
-import { ref } from "vue";
+import '@milaboratories/graph-maker/styles';
 import { useApp } from "../app";
 
 const app = useApp();
 
-const settings = ref({
-    title: "Diversity Analysis",
-    chartType: "discrete",
-    template: "box"
-} satisfies GraphMakerSettings);
+// @TODO move to defaultUi when implemented in the model
+if (app.ui.graphState === undefined) {
+    app.model.ui.graphState = {
+        title: "Diversity Analysis",
+        chartType: "discrete",
+        template: "box"
+    } satisfies GraphMakerSettings
+};
 </script>
 
 <template>
-    <GraphMaker :pFrame="app.model.outputs.pf" :modelValue="settings" />
+    <GraphMaker :pFrame="app.model.outputs.pf" v-model="app.model.ui.graphState" />
 </template>
