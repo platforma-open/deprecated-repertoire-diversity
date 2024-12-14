faa5bc9a581c4d58dc5c9e38b407ba6c999488b2
Stanislav Poslavsky <poslavskysv@milaboratories.com>
2024-12-14 12:33:49 +0100

    wip

--- ui/src/pages/MainPage.vue
+++ ui/src/pages/MainPage.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { PTableColumnSpec } from '@platforma-sdk/model';
-import { PlAgDataTable, PlAgDataTableController, PlAgDataTableToolsPanel, PlBlockPage, PlBtnGhost, PlDataTableSettings, PlEditableTitle, PlMaskIcon24 } from '@platforma-sdk/ui-vue';
+import { PlAgDataTable, PlAgDataTableToolsPanel, PlBlockPage, PlBtnGhost, PlDataTableSettings, PlEditableTitle, PlMaskIcon24 } from '@platforma-sdk/ui-vue';
 import { computed, ref } from 'vue';
 import { useApp } from '../app';
 import SettingsModal from './SettingsModal.vue';
@@ -16,7 +16,6 @@ const tableSettings = computed<PlDataTableSettings>(() => ({
 const settingsAreShown = ref(app.model.outputs.pt === undefined)
 const showSettings = () => { settingsAreShown.value = true }
 const columns = ref<PTableColumnSpec[]>([]);
-const tableInstance = ref<PlAgDataTableController>();
 </script>
 
 <template>
@@ -26,12 +25,6 @@ const tableInstance = ref<PlAgDataTableController>();
     </template>
     <template #append>
       <PlAgDataTableToolsPanel />
-      <PlBtnGhost @click.stop="() => tableInstance?.exportCsv()">
-        Export
-        <template #append>
-          <PlMaskIcon24 name="export" />
-        </template>
-      </PlBtnGhost>
 
       <PlBtnGhost @click.stop="showSettings">Settings
         <template #append>
@@ -40,7 +33,7 @@ const tableInstance = ref<PlAgDataTableController>();
       </PlBtnGhost>
     </template>
     <PlAgDataTable v-if="app.model.ui" :settings="tableSettings" v-model="app.model.ui.tableState" show-columns-panel
-      ref="tableInstance" @columns-changed="(newColumns) => (columns = newColumns)" />
+      show-export-button ref="tableInstance" @columns-changed="(newColumns) => (columns = newColumns)" />
   </PlBlockPage>
 
   <SettingsModal v-model="settingsAreShown" />
