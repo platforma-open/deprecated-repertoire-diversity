239c6f626b2d11311454032bba776df5d265c2df
Vadim Piven <vadim.piven@milaboratories.com>
2024-12-16 20:03:34 +0400

    Dependancy update

---
+++ .changeset/chilled-crabs-repair.md
@@ -0,0 +1,5 @@
+---
+'@platforma-open/milaboratories.repertoire-diversity.ui': patch
+---
+
+Dependancy update
--- ui/src/pages/GraphPage.vue
+++ ui/src/pages/GraphPage.vue
@@ -1,54 +1,58 @@
 <script setup lang="ts">
-import { GraphMaker, GraphMakerProps } from "@milaboratories/graph-maker";
+import { GraphMaker, GraphMakerProps } from '@milaboratories/graph-maker';
 import '@milaboratories/graph-maker/styles';
-import { useApp } from "../app";
+import { useApp } from '../app';
 
 const app = useApp();
 
 const defaultOptions: GraphMakerProps['defaultOptions'] = [
-    {
-        inputName: 'y',
-        selectedSource: {
-            kind: 'PColumn',
-            valueType: 'Float',
-            name: 'pl7.app/vdj/diversity',
-            axesSpec: [
-                {
-                    type: 'String',
-                    name: 'pl7.app/sampleId'
-                },
-                {
-                    type: 'String',
-                    name: 'pl7.app/vdj/diversityMeasure'
-                }
-            ]
+  {
+    inputName: 'y',
+    selectedSource: {
+      kind: 'PColumn',
+      valueType: 'Float',
+      name: 'pl7.app/vdj/diversity',
+      axesSpec: [
+        {
+          type: 'String',
+          name: 'pl7.app/sampleId'
+        },
+        {
+          type: 'String',
+          name: 'pl7.app/vdj/diversityMeasure'
         }
-    },
-    {
-        inputName: 'facetBy',
-        selectedSource: {
-            type: "String",
-            name: "pl7.app/vdj/chain"
-        }
-    },
-    {
-        inputName: 'tabBy',
-        selectedSource: {
-            type: "String",
-            name: "pl7.app/vdj/diversityMeasure"
-        }
-    },
-    {
-        inputName: 'primaryGrouping',
-        selectedSource: {
-            type: "String",
-            name: "pl7.app/sampleId"
-        }
+      ]
     }
-]
+  },
+  {
+    inputName: 'facetBy',
+    selectedSource: {
+      type: 'String',
+      name: 'pl7.app/vdj/chain'
+    }
+  },
+  {
+    inputName: 'tabBy',
+    selectedSource: {
+      type: 'String',
+      name: 'pl7.app/vdj/diversityMeasure'
+    }
+  },
+  {
+    inputName: 'primaryGrouping',
+    selectedSource: {
+      type: 'String',
+      name: 'pl7.app/sampleId'
+    }
+  }
+];
 </script>
 
 <template>
-    <GraphMaker chart-type="discrete" :p-frame="app.model.outputs.pf" v-model="app.model.ui.graphState"
-        :default-options="defaultOptions" />
+  <GraphMaker
+    chart-type="discrete"
+    :p-frame="app.model.outputs.pf"
+    v-model="app.model.ui.graphState"
+    :default-options="defaultOptions"
+  />
 </template>
